65ea6d3c566fd284fb8c931f88385950029eb197
Daniel M. Drucker <dmd@3e.org>
2025-02-08 10:56:11 -0500

    np.where

--- bdf.py
+++ bdf.py
@@ -4,30 +4,32 @@ from bdflib import reader
 import numpy as np
 import os, sys
 
+
 class bdf:
     def __init__(self, fontfilename):
-        self.bdffont = reader.read_bdf(open(fontfilename, 'rb'))
+        self.bdffont = reader.read_bdf(open(fontfilename, "rb"))
 
     def letter(self, let):
         character = self.bdffont.glyphs_by_codepoint[ord(let)].data
-        return [list('{0:0=8b}'.format(row).replace('0','.').replace('1','x')) for row in reversed(character)]
+        return [
+            list("{0:0=8b}".format(row).replace("0", ".").replace("1", "x"))
+            for row in reversed(character)
+        ]
 
     def trim_letter(self, let):
         im = np.array(self.letter(let))
-
         height, width = np.shape(im)
 
-        # find the first and last column with a black pixel
-        # np.where or something like that would probably work better but who cares
-        for column_first in range(width):
-            if 'x' in im[:, column_first]:
-                break
+        x_indices = np.where(im == "x")
+        if len(x_indices[1]) == 0:
+            return np.array(list("." * height))[:, np.newaxis]
 
-        for column_last in reversed(range(width)):
-            if 'x' in im[:, column_last]:
-                break
+        column_first = np.min(x_indices[1])
+        column_last = np.max(x_indices[1])
 
-        assert(column_last + 1 <= width)
-        return np.hstack((im[:, column_first:column_last+1],np.array(list('.' * height))[:, np.newaxis]))
-
-
+        return np.hstack(
+            (
+                im[:, column_first : column_last + 1],
+                np.array(list("." * height))[:, np.newaxis],
+            )
+        )
